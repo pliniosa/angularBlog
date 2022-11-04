54f90c8b6f054b4fcfd2553339f57b53b2846524
pliniosa <plinioaredes@gmail.com>
2022-11-04 20:46:34 -0300

    atualizacao 1.2

--- src/app/componentes/big-card/big-card.component.ts
+++ src/app/componentes/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,13 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+  imagem: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  cardDescription: string = ""
+  @Input()
+  link: string = ""
   constructor() { }
 
   ngOnInit(): void {
   }
-
-
-  toLink(){
-    let big_card_link = "https://vidafullstack.com.br/angular/novidades-no-angular-v14/"
+  toLink() {
+    let big_card_link = ""
   }
 }
--- src/app/componentes/small-card/small-card.component.ts
+++ src/app/componentes/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,12 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input()
+  imagem: string = ""
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  link: string = ""
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  toLink(){
-    let big_card_link = "https://blog.schoolofnet.com/como-comecar-com-node-js/"
   }
 }
